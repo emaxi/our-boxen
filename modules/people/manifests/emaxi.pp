db68337ddaee1b88e7d44d1b67cfad574aae3aee
Maximiliano Gonzalez <maxi@Maxis-Mac-mini.local>
2014-05-06 09:38:41 -0300

    Customize site.pp and add emaxi to people

---
+++ modules/people/manifests/emaxi.pp
@@ -0,0 +1,13 @@
+class people::emaxi { 
+     
+    #
+    # Remove services we don't want
+    #
+    service {"dev.nginx":
+        ensure => "stopped",
+    }
+   
+    service {"dev.dnsmasq":
+        ensure => "stopped",
+    }
+ }
